67c806366dc4ff79c75dbb64fd31c5697de71b90
trungnotchung <trung09072004@gmail.com>
2025-07-28 13:27:57 +0700

    add config for eth and pt contract

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,14 @@ function loadConfig() {
 		port: process.env.PORT || 3000,
 		db: {
 			url: process.env.DB_URL || 'mongodb://localhost:27017/pt-balance',
+		},
+		eth: {
+			rpcUrl: process.env.ETH_RPC_URL || 'wss://ethereum-rpc.publicnode.com',
+		},
+		contracts: {
+			pt: {
+				address: process.env.PT_ADDRESS || '',
+			}
 		}
 	}
 }
