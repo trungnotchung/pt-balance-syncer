42d09592b86e60f97fc70ebe172d5542c8677e7d
trungnotchung <0xtrungnotchung@gmail.com>
2025-07-30 10:27:05 +0700

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { appConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('PT Balance API')
+    .setDescription('The PT Balance API description')
+    .setVersion('0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, swaggerDocument, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(appConfig().port);
 }
 bootstrap();
